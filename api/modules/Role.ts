72e24de1f5f77da3e3599ea4f9c0c2084ca73907
Winna-wnn <68949293+Winna-wnn@users.noreply.github.com>
2023-02-17 13:26:00 +0800

    Add files via upload

--- api/modules/Role.ts
+++ api/modules/Role.ts
@@ -1,1 +1,10 @@
+import { http } from '@/http'
+import { BasicPageParams } from '@/types'
+import {
+  GetList_GetListResponse
+} from '@/types/modules/Role'
 
+export const GetList = () => {
+  return http.request<GetList_GetListResponse>('get', '/api/Role/GetList', {
+  })
+}
