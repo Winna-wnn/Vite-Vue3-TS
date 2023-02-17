69b8c117fa27641a615150bea6d0c0390c4f848a
Winna-wnn <68949293+Winna-wnn@users.noreply.github.com>
2023-02-17 13:35:33 +0800

    Add files via upload

---
+++ types/shims-vue.d.ts
@@ -0,0 +1,15 @@
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+declare module '*.scss' {
+  const scss: Record<string, string>
+  export default scss
+}
+
+declare module 'vuedraggable/src/vuedraggable'
+
+declare module 'lodash'
+declare module 'lodash-es'
