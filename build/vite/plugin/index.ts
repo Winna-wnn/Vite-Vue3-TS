35493e92af4a0d1b41d349911ead0b4a23d5dcb1
Winna-wnn <68949293+Winna-wnn@users.noreply.github.com>
2023-02-17 13:34:39 +0800

    Add files via upload

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -1,1 +1,24 @@
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { PluginOption } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import configSvgIconsPlugin from './svg'
+import { configThemePlugin } from './theme'
+export function getPluginsList(
+  isBuild: boolean
+): (PluginOption | PluginOption[])[] {
 
+  const vitePlugins: (PluginOption | PluginOption[])[] = [
+    vue(),
+    vueJsx(),
+    createHtmlPlugin()
+  ]
+
+  // vite-plugin-svg-icons
+  vitePlugins.push(configSvgIconsPlugin(isBuild))
+
+  // vite-plugin-theme
+  vitePlugins.push(configThemePlugin(isBuild))
+
+  return vitePlugins
+}
